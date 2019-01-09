7f899bcb4516eeba2d98e49534de76a18c45d6da
Louis GE <gejian18@mails.ucas.edu.cn>
2019-01-09 08:51:04 +0000

    安全等级bug修改

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,8 @@ Coderwall::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.serve_static_assets = true
-  config.assets.compile = true
+  config.serve_static_files = true
+  config.assets.compile = false
   config.assets.compress = true
   config.assets.digest = true
   config.static_cache_control = 'public, max-age=31536000'
