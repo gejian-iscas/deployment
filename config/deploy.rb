5518ab5e65d38e99e2e985478cd403fb14fdc977
jian ge <louis0526@vultr.guest>
2019-01-13 14:25:02 +0000

    修改服务器设置

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/louis0526/deployment.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/louis0526/workspace/deployment"
+set :deploy_to, "/home/louis0526/workspace/publish/codeknows"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "39.106.60.115", user: "louis0526", roles: %w{app db web}, my_property: :my_value
+server "45.76.56.242", user: "louis0526", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
